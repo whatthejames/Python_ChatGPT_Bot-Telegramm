d07691462722b05bfae1c1b712a869af4be7f545
DeepCoreB4 <cupparikun@gmail.com>
2022-12-30 00:29:31 +0100

    edit readMe, chat_telegram.py -  exited with code=1 / fix

--- README.md
+++ README.md
@@ -13,6 +13,27 @@ To use ChatGPT, you will need a Telegram account and the Telegram app (available
 
 To start chatting with ChatGPT, simply send it a message and it will respond with a generated response. You can ask ChatGPT questions, have a conversation with it, or just have fun chatting. ChatGPT is capable of understanding and responding to a wide variety of topics, so feel free to engage with it on any subject that interests you.
 
+## Setup in VSCODE
+1.  Clone Repositori from given link
+    https://github.com/DeepCoreB4/Phyton_ChatGPT_Bot-Telegramm
+2.  Rename the example.env file to .env and open it. Edit the 1.Line/ OPENAI_API_KEY with =<YOUR-OPENAI_API_KEY>
+    Also add in line 2. your TELEGRAM_TOKEN =<YOUR-TELEGRAM-API-TOKEN>
+3.  If you dont now your openAI API-KEY go to https://beta.openai.com/overview if necessary log in. Visit this link
+    https://beta.openai.com/account/api-keys and click [+ Create new secret key] this generates your unique OPENAI-API-KEY
+4.  Open an PowerShell Terminal in your workspace folder and install NPM with 'npm install'
+
+Now we need to install the following packages to us our bot!
++ openai
++ pytho-telegram-bot
++ python-dotenv
+
+6.  Open Terminal again and install [python-dotenv] with this command <pip install python-dotenv> if you get errors you may try to upgrade your [python-dotenv] 
+    with <pip install --upgrade python-dotenv> and this command <pip install --upgrade pip>
+    "If it still doesn't work! Could it be that Python is not installed on your system or is not up to date? In that case, you need to install the latest version of Python. You can do this from here https://www.python.org/downloads/"
+7.  after that you need to install with this <pip install openai>   
+8.  Then you can install with the following command <pip install python-telegram-bot>
+
+
 ### Disclaimer
 Please note that ChatGPT is a chatbot and is not a human. Its responses are generated using advanced artificial intelligence algorithms, but it may not always produce responses that are accurate, appropriate, or sensitive to your individual circumstances. Use ChatGPT at your own risk, and always use common sense and good judgment when interacting with it or any other chatbot.
 
--- chatgpt_telegram.py
+++ chatgpt_telegram.py
@@ -1,21 +1,21 @@
+import os
+import logging
 import openai
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import logging
 from dotenv import load_dotenv
-import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Lädt die Umgebungsvariablen aus der .env-Datei
 load_dotenv()
 
 # Verwendet den Wert der Umgebungsvariablen "DEBUG"
-debug = os.getenv("DEBUG")
+debug = os.environ.get("DEBUG", False)
 
 logger = logging.getLogger(__name__)
 
 # Verbindung zur OpenAI-API herstellen und ChatGPT-Modell angeben
 # Verwendet den Wert der Umgebungsvariablen "OPENAI_API_KEY"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 model_engine = "chatgpt"
 
@@ -46,28 +46,37 @@ def chat(update, context):
     response = generate_response(text)
     update.message.reply_text(response)
 
-# Telegram-Bot starten und Nachrichten verarbeiten
-updater = Updater(os.getenv("TELEGRAM_TOKEN"), use_context=True)
-dispatcher = updater.dispatcher
-
-start_handler = CommandHandler("start", start)
-dispatcher.add_handler(start_handler)
-
-chat_handler = MessageHandler(Filters.text, chat)
-dispatcher.add_handler(chat_handler)
-
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-# Error-Handler hinzufügen
-dispatcher.add_error_handler(error)
+def main():
+    # Telegram-Bot starten und Nachrichten verarbeiten
+    token = os.environ.get("TELEGRAM_TOKEN")
+    if not token:
+        logger.error("TELEGRAM_TOKEN environment variable is not set")
+        return
 
-try:
-    # Den Bot starten
-    updater.start_polling()
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
 
-    # Auf Cleanup-Interrupts reagieren (z.B. STRG+C oder SIGTERM)
-    updater.idle()
-except Exception as e:
-    print("An error occurred:", e)
+    start_handler = CommandHandler("start", start)
+    dispatcher.add_handler(start_handler)
+
+    chat_handler = MessageHandler(Filters.text, chat)
+    dispatcher.add_handler(chat_handler)
+
+    # Error-Handler hinzufügen
+    dispatcher.add_error_handler(error)
+
+    try:
+        # Den Bot starten
+        updater.start_polling()
+
+        # Auf Cleanup-Interrupts reagieren (z.B. STRG+C oder SIGTERM)
+        updater.idle()
+    except Exception as e:
+        logger.exception("An error occurred: %s", e)
+
+if __name__ == "__main__":
+    main()
