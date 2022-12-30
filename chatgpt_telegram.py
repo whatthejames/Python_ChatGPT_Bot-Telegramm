b7b2900ac0429ddf7af6c1570fa2ee7cf76ff8ae
DeepCoreB4 <cupparikun@gmail.com>
2022-12-30 20:39:07 +0100

    ...

--- chatgpt_telegram.py
+++ chatgpt_telegram.py
@@ -1,16 +1,5 @@
 from openai_functions import generate_response
 from telegram_functions import start, chat, error
-import os
-import logging
-from dotenv import load_dotenv
-
-# Lädt die Umgebungsvariablen aus der .env-Datei
-load_dotenv()
-
-# Verwendet den Wert der Umgebungsvariablen "DEBUG"
-debug = os.environ.get("DEBUG", False)
-
-logger = logging.getLogger(__name__)
 
 def main():
     # Code um Telegram-Bot zu starten und Nachrichten zu verarbeiten
@@ -21,4 +10,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
