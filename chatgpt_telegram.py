2c2495c9142d6fbc49a28725b4ec7b1becd06102
DeepCoreB4 <cupparikun@gmail.com>
2022-12-30 19:09:19 +0100

    not working :(

--- chatgpt_telegram.py
+++ chatgpt_telegram.py
@@ -1,12 +1,25 @@
-from openai_functions import generate_response
-from telegram_functions import start, chat, error
 import os
 import logging
-#import telegram #/ ausgelagert in telegram_function.py
+import openai
+import telegram
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters #/ ausgelagert in telegram_function.py
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-#Funktion, die ChatGPT verwendet, um auf eine Nachricht zu antworten
+# Lädt die Umgebungsvariablen aus der .env-Datei
+load_dotenv()
+
+# Verwendet den Wert der Umgebungsvariablen "DEBUG"
+debug = os.environ.get("DEBUG", False)
+
+logger = logging.getLogger(__name__)
+
+# Verbindung zur OpenAI-API herstellen und ChatGPT-Modell angeben
+# Verwendet den Wert der Umgebungsvariablen "OPENAI_API_KEY"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+model_engine = "chatgpt"
+
+# Funktion, die ChatGPT verwendet, um auf eine Nachricht zu antworten
 def generate_response(text):
     prompt = (f"User: {text}\n"
              f"MicroBot: ")
@@ -24,11 +37,10 @@ def generate_response(text):
     message = completions.choices[0].text
     return message.strip()
 
-logger = logging.getLogger(__name__)
-
-#Funktionen für den Telegram-Chatbot
+# Funktionen für den Telegram-Chatbot
 def start(update, context):
     update.message.reply_text("Hi, ich bin der Micro-ChatBot von DeepCore Developers integriert mit ChatGPT. Schreib mir eine Nachricht, und ich werde versuchen, dir zu antworten.")
+
 def chat(update, context):
     text = update.message.text
     response = generate_response(text)
@@ -66,5 +78,5 @@ def main():
     except Exception as e:
         logger.exception("An error occurred: %s", e)
 
-#if __name__ == "__main__":
-#   main()
+if __name__ == "__main__":
+    main()
