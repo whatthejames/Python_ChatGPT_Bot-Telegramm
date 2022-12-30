ec45d2698bdd1fb66195332e502420085f441470
DeepCoreB4 <cupparikun@gmail.com>
2022-12-30 21:37:36 +0100

    ...

--- openai_functions.py
+++ openai_functions.py
@@ -1,4 +1,22 @@
-import os,openai as A
-A.api_key=os.environ.get('OPENAI_API_KEY')
-B='chatgpt'
-def C(text):B=f"User: {text}\nMicroBot: ";C=A.Completion.create(model='text-davinci-003',prompt=B,max_tokens=1024,n=1,stop=None,temperature=0.5,top_p=0.3,frequency_penalty=0.5,presence_penalty=0);D=C.choices[0].text;return D.strip()
+import os
+import openai
+# Verwendet den Wert der Umgebungsvariablen "OPENAI_API_KEY"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+model_engine = "chatgpt"
+# Funktion, die ChatGPT verwendet, um auf eine Nachricht zu antworten
+def generate_response(text):
+    prompt = (f"User: {text}\n"
+             f"MicroBot: ")
+    completions = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        top_p=0.3,
+        frequency_penalty=0.5,
+        presence_penalty=0
+    )
+    message = completions.choices[0].text
+    return message.strip()
--- telegram_functions.py
+++ telegram_functions.py
@@ -1,16 +1,41 @@
-import logging as B,os,telegram
-from telegram.ext import Updater as E,CommandHandler as F,MessageHandler as G,Filters as H
-from dotenv import load_dotenv as C
-C()
-L=os.environ.get('DEBUG',False)
-A=B.getLogger(__name__)
-def I(update,context):update.message.reply_text('Hi, ich bin der Micro-ChatBot von DeepCore Developers integriert mit ChatGPT. Schreib mir eine Nachricht, und ich werde versuchen, dir zu antworten.')
-def J(update,context):A=update;B=A.message.text;C=generate_response(B);A.message.reply_text(C)
-def K(update,context):A.warning('Update "%s" caused error "%s"',update,context.error)
-def D():
-	D=os.environ.get('TELEGRAM_TOKEN')
-	if not D:A.error('TELEGRAM_TOKEN environment variable is not set');return
-	B=E(D,use_context=True);C=B.dispatcher;L=F('start',I);C.add_handler(L);M=G(H.text,J);C.add_handler(M);C.add_error_handler(K)
-	try:B.start_polling();B.idle()
-	except Exception as N:A.exception('An error occurred: %s',N)
-if __name__=='__main__':D()
+import logging
+import os
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from dotenv import load_dotenv
+# Lädt die Umgebungsvariablen aus der .env-Datei
+load_dotenv()
+debug = os.environ.get("DEBUG", False)
+logger = logging.getLogger(__name__)
+# Funktionen für den Telegram-Chatbot
+def start(update, context):
+    update.message.reply_text("Hi, ich bin der Micro-ChatBot von DeepCore Developers integriert mit ChatGPT. Schreib mir eine Nachricht, und ich werde versuchen, dir zu antworten.")
+def chat(update, context):
+    text = update.message.text
+    response = generate_response(text)
+    update.message.reply_text(response)
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def main():
+    # Telegram-Bot starten und Nachrichten verarbeiten
+    token = os.environ.get("TELEGRAM_TOKEN")
+    if not token:
+        logger.error("TELEGRAM_TOKEN environment variable is not set")
+        return
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler("start", start)
+    dispatcher.add_handler(start_handler)
+    chat_handler = MessageHandler(Filters.text, chat)
+    dispatcher.add_handler(chat_handler)
+    # Error-Handler hinzufügen
+    dispatcher.add_error_handler(error)
+    try:
+        # Den Bot starten
+        updater.start_polling()
+        updater.idle()
+    except Exception as e:
+        logger.exception("An error occurred: %s", e)
+if __name__ == "__main__":
+    main()
