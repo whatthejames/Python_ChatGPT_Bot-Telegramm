2725a60c465a07a1dab0628a673bbf0b0790ee3a
DeepCoreB4 <cupparikun@gmail.com>
2022-12-31 03:23:23 +0100

    ...B

--- chatgpt_telegram.py
+++ chatgpt_telegram.py
@@ -20,8 +20,8 @@ model_engine = "chatgpt"
 
 # Funktion, die ChatGPT verwendet, um auf eine Nachricht zu antworten
 def generate_response(text):
-    prompt = (f"User:(text)\n"
-             f"MicroBot:"),
+    prompt = (f"User: {text}\n"
+             f"MicroBot: ")
     completions = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
