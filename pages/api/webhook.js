bba7715313f90c77f73671a7cfc61c683ff4ce19
DeepCoreB4 <cupparikun@gmail.com>
2022-12-31 01:26:07 +0100

    ..

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,11 +1,3 @@
-module.exports = (request, response) => {
-    response.json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
-};
-
 export default function handler(request, response) {
     response.status(200).json({
         body: request.body,
