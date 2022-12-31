613a889c94fad9ea964cb6912940da66e55a12b0
DeepCoreB4 <cupparikun@gmail.com>
2022-12-31 01:25:49 +0100

    .-

--- api/webhook.js
+++ api/webhook.js
@@ -5,11 +5,3 @@ module.exports = (request, response) => {
         cookies: request.cookies,
     });
 };
-
-export default function handler(request, response) {
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
-}
