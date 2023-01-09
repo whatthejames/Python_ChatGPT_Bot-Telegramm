5c7e12cf7bc7f205355d3f1ec61ee2dbe23a0708
DeepCoreB4 <cupparikun@gmail.com>
2023-01-09 20:54:17 +0100

    ....

--- chatgpt_telegram.py
+++ chatgpt_telegram.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def generate_response(text):
-    prompt = (f"User {text}: \n"
+    prompt = ((f"User:{text}\n")
     f"MicroBot: ")
     completions = openai.Completion.create(
         model="text-davinci-003",
