69cb329f8f0819791b6fb6cdb25d317ef0a0b855
DeepCoreB4 <cupparikun@gmail.com>
2023-01-10 16:29:38 +0100

    ....

--- chatgpt_telegram.py
+++ chatgpt_telegram.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 # Funktion, die ChatGPT verwendet, um auf eine Nachricht zu antworten
 def generate_response(text):
-        prompt = (f'User:{text}\n
+        prompt = (f'User:{text}\n,
             f'MicroBot: ')
     completions = openai.Completion.create(
         model="text-davinci-003",
